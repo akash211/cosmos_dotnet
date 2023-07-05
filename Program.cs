58f03c3de1462e60c114f3c0532c169b442a5a56
Akash Kumar <akash211@gmail.com>
2023-07-05 18:30:13 +0530

    emulator working

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool offline = true;
+            string? endpoint; // ? makes the type nullable, default is non-nullable
+            string? key;
+            CosmosClient client;
+            // We can change options to client as well
+            CosmosClientOptions options =
+                new()
+                {
+                    ConnectionMode = ConnectionMode.Direct, // can be Direct ot Gateway. Default is Direct. In Gatway, requests are routed through database gateway as proxy
+                    ConsistencyLevel = ConsistencyLevel.Eventual, // to change consistency level, total 5 consistency levels. It is used to weaken read consistency.
+                    ApplicationRegion = Regions.EastUS, // if multi-write is not enabled, it only for read operations
+                    // ApplicationPreferredRegions = new List<string> { "westus", "eastus" }
+                };
+            if (offline == false)
+            {
+                Console.WriteLine($"Azure {offline}");
+                endpoint = Environment.GetEnvironmentVariable("COSMOS_ENDPOINT");
+                key = Environment.GetEnvironmentVariable("COSMOS_KEY");
+                client = new(endpoint, key, options);
+            }
+            else
+            {
+             Console.WriteLine($"Emulator {offline}");
+                endpoint = "https://localhost:8081/";
+                key =
+                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+                client = new(endpoint, key);
+            } // using  /Key new key also can be generated but by default this is static
+
+            AccountProperties account = await client.ReadAccountAsync();
+            Console.WriteLine($"Account Name:\t{account.Id}");
+            Console.WriteLine($"Primary Region:\t{account.WritableRegions.FirstOrDefault()?.Name}");
+
+            Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+            // Database database = client.GetDatabase("cosmicworks"); # database exits then just to retrive it
+            // Database database = await client.CreateDatabaseAsync("cosmicworks"); # If database does not exist and we have to create
+            Container container = await database.CreateContainerIfNotExistsAsync(
+                id: "products",
+                partitionKeyPath: "/categoryId",
+                throughput: 400
+            ); // same database commands work for containers as well.
+            Console.WriteLine($"New database:\t{database.Id}");
+            Console.WriteLine($"New container:\t{container.Id}");
+        }
+    }
+}
--- script_to_run.ps1
+++ script_to_run.ps1
@@ -4,95 +4,56 @@
 # Import-Module -Name AzureRM
 
 # To check version on Powershell
-Write-Output $PSVersionTable.PSVersion
+# Write-Output $PSVersionTable.PSVersion
 
 # Check if AzureRM is installed
-Get-Module -Name AzureRM -ListAvailable | Write-Host -ForegroundColor Red
+# Get-Module -Name AzureRM -ListAvailable | Write-Host -ForegroundColor Red
 
 # Check and modify execution policy
-Write-Output "Modifying Execution Policy for RemoteSigned and printing"
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Get-ExecutionPolicy -List | Write-Host -ForegroundColor Green
+# Write-Output "Modifying Execution Policy for RemoteSigned and printing"
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# Get-ExecutionPolicy -List | Write-Host -ForegroundColor Green
 
 # To install Az Powershell ( -AllowClobber may not be required here)
 # Install-Module -Name Az -Repository PSGallery -Force -AllowClobber
 
 # Updating az module
-Write-Output "Updating Az"
-Update-Module -Name Az -Force
-Write-Output "Az Updated"
+# Write-Output "Updating Az"
+# Update-Module -Name Az -Force
+# Write-Output "Az Updated"
 
 # To login to Azure
-Connect-AzAccount  | Write-Host -ForegroundColor Red
-# This returns:
-# Account            SubscriptionName TenantId                             Environment
-# -------            ---------------- --------                             -----------
-# akash211@gmail.com pay_subscription 979906ec-f50e-4d57-b9fd-0be4add3d129 AzureCloud
+Connect-AzAccount
 
-# Variable for resource group name
-$RESOURCE_GROUP_NAME = "msdocs-cosmos-quickstart-rg"
+# Variables
+$RESOURCE_GROUP_NAME = "cosmos-420-rg"
 $LOCATION = "West US"
+$ACCOUNT_NAME = "cert-420"
 
-# Variable for account name with a randomly generated suffix
-$SUFFIX = Get-Random
-$ACCOUNT_NAME = "msdocs-$SUFFIX"
-
-$parameters = @{
-    Name = $RESOURCE_GROUP_NAME
-    Location = $LOCATION
-}
-New-AzResourceGroup @parameters | Write-Host -ForegroundColor Blue
-# We get below result:
-# ResourceGroupName : msdocs-cosmos-quickstart-rg
-# Location          : westus
-# ProvisioningState : Succeeded
-# Tags              : 
-# ResourceId        : /subscriptions/22df909b-26e6-4a40-adac-05e51cd2aabe/resourceGroups/msdocs-cosmos-quickstart-rg
+# $parameters = @{
+#     Name = $RESOURCE_GROUP_NAME
+#     Location = $LOCATION
+# }
+# New-AzResourceGroup @parameters
 
 $parameters = @{
     ResourceGroupName = $RESOURCE_GROUP_NAME
-    Name = $ACCOUNT_NAME
-    Location = $LOCATION
-    EnableFreeTier = 0
+    Name              = $ACCOUNT_NAME
+    Location          = $LOCATION
+    EnableFreeTier    = 0
 }
-New-AzCosmosDBAccount @parameters  | Write-Host -ForegroundColor Blue
-# Here received this result:
-# Capabilities                       : {}
-# ConsistencyPolicy                  : Microsoft.Azure.Management.CosmosDB.Models.ConsistencyPolicy
-# EnableAutomaticFailover            : False
-# IsVirtualNetworkFilterEnabled      : False
-# IpRules                            : {}
-# DatabaseAccountOfferType           : Standard
-# DocumentEndpoint                   : https://msdocs-285412314.documents.azure.com:443/
-# ProvisioningState                  : Succeeded
-# Kind                               : GlobalDocumentDB
-# ConnectorOffer                     : 
-# DisableKeyBasedMetadataWriteAccess : False
-# PublicNetworkAccess                : Enabled
-# KeyVaultKeyUri                     : 
-# PrivateEndpointConnections         : 
-# EnableFreeTier                     : False
-# ApiProperties                      : Microsoft.Azure.Commands.CosmosDB.Models.PSApiProperties
-# EnableAnalyticalStorage            : False
-# EnablePartitionMerge               : False
-# NetworkAclBypass                   : None
-# NetworkAclBypassResourceIds        : {}
-# InstanceId                         : 264dd636-b34f-4492-8a0f-d79ab2737684
-# BackupPolicy                       : Microsoft.Azure.Commands.CosmosDB.Models.PSBackupPolicy
-# RestoreParameters                  : Microsoft.Azure.Commands.CosmosDB.Models.PSRestoreParameters
-# CreateMode                         : 
-# AnalyticalStorageConfiguration     : Microsoft.Azure.Commands.CosmosDB.Models.PSAnalyticalStorageConfiguration
+New-AzCosmosDBAccount @parameters
 
 $parameters = @{
     ResourceGroupName = $RESOURCE_GROUP_NAME
     Name = $ACCOUNT_NAME
 }
 Get-AzCosmosDBAccount @parameters |
-    Select-Object -Property "DocumentEndpoint"  | Write-Host -ForegroundColor Green
+    Select-Object -Property "DocumentEndpoint"
 # Now result is below:
 #     DocumentEndpoint
 # ----------------
-# https://msdocs-173608580.documents.azure.com:443/
+# https://cert-420.documents.azure.com:443/
 
 $parameters = @{
     ResourceGroupName = $RESOURCE_GROUP_NAME
@@ -100,12 +61,9 @@ $parameters = @{
     Type = "Keys"
 }    
 Get-AzCosmosDBAccountKey @parameters |
-    Select-Object -Property "PrimaryMasterKey" | Write-Host -ForegroundColor Red
-# Here is the result:
-# PrimaryMasterKey
-# ----------------
-# u4HvMWqBY7yfetWjlnsbMF1c1yCxDX97RTTAxAFqFiBjfu4nh5PYjb5MhixOYd38kx7Plr4eLmw5ACDbgm9WpQ==
+   Select-Object -Property "PrimaryMasterKey" 
 
-# custom role definition id - 7063662f-1228-4029-9e6a-c0b6da63c596
+
+# custom role definition id - d44631fa-7e74-48ad-98ae-fd3b637aee01
 # my prinicpal email id - akash211_gmail.com#EXT#@akash211gmail.onmicrosoft.com
 # principal id - 0c2f731e-6cc2-4bf4-9a74-37fd5c76a1ba
